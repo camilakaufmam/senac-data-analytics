fdfaeb879eabf74d26960a1e7ccf339a4bcca86f
camilakaufmam <camilakaufmam@gmail.com>
2026-03-17 21:34:59 -0300

    condicionais

--- Aula12.Cristian/condicionais.py
+++ Aula12.Cristian/condicionais.py
@@ -17,8 +17,28 @@
 #EXERCICIO 2:
 #Crie um algoritmo para saber se um usuário é maior ou menor de idade;
 
+# idade = int(input("Qual sua idade? "))
+
+# if idade >= 18:
+#     print("Você é maior de idade")
+# else: 
+#     print("Você é menor de idade")
+
+#EXERCICIO 3:
+#Crie um algoritmo que pergunte a idade;
+#se a idade for menor que 12, então ecreva "Você é um criança";
+#se a idade for menor que 18, então ecreva "Você é um adolescente";
+#se a idade for menor que 70, então ecreva "Você é um adulto";
+#senão, escreva "Você é um idoso";
+
+
 idade = int(input("Qual sua idade? "))
 
-if idade >= 18:
-    print("Você é maior de idade")
-else: print("Você é menor de idade")
+if idade <= 12:
+    print("Você é um criança")
+elif idade <= 18:
+    print("Você é um adolescente")
+elif idade <=70:
+    print("Você é um adulto")
+else:
+    print("Você é um idoso")
